48dbae47d6c6db1a57d8a4fe1d00b6a269f5544e
abdulmomen.ghalkha <abdulmomen.ghalkha@gmail.com>
2024-12-31 15:03:28 +0200

    Fix the dataset creation, To-Do: data splitting

--- data_feed.py
+++ data_feed.py
@@ -47,3 +47,45 @@ class DataFeed(Dataset):
             img = self.transform(img)
         label = sample[1]
         return (img,label)
+
+
+############### Create data sample list #################
+def create_data_sample(data_csv, shuffle=False, nat_sort=False):
+    data_samples = []
+    pred_val = []
+    for idx, row in data_csv.iterrows():
+        img_paths = row.values[5:7]
+        features = row.values[1:5]
+        data_samples.append([features, img_paths])
+    return data_samples
+#############################################################
+
+class DataFeed_image_pos(Dataset):
+    '''
+    A class retrieving a tuple of (image,label). It can handle the case
+    of empty classes (empty folders).
+    '''
+    def __init__(self,data_csv, nat_sort = False, transform=None, init_shuflle = True, modalities=["pos_height", "images"]):
+        self.datacsv = data_csv
+        self.samples = create_data_sample(self.datacsv,shuffle=init_shuflle,nat_sort=nat_sort)
+        self.transform = transform
+        self.modalities = modalities
+
+
+    def __len__(self):
+        return len( self.samples )
+
+    def __getitem__(self, idx):
+        sample = self.samples[idx]
+        pos = sample[0].astype(np.float32)
+        img = io.imread(sample[1][0])
+        if self.transform and "images" in self.modalities:
+            img = self.transform(img)
+        label = sample[1][1]
+        if "pos_height" in self.modalities and "images" in self.modalities:
+            return (pos, img, label)
+        elif "pos_height" in self.modalities:
+            return (pos,label)
+        else:
+            return (img,label)
+
